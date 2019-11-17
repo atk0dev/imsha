91665e1a3ba7f751fac8cb242e279928721a5a66
Andrii <atk0@outlook.com>
2019-11-17 14:19:29 +0200

    Connecton to database

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 imsha
+
+npm run server
--- server.js
+++ server.js
@@ -7,19 +7,21 @@ const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 
 require("dotenv").config({ path: "variables.env" });
+
 const User = require("./models/User");
 const Post = require("./models/Post");
 
 mongoose
-  .connect(
-    process.env.MONGO_URI,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
 const server = new ApolloServer({
   typeDefs,
+  resolvers,
   context: {
     User,
     Post
